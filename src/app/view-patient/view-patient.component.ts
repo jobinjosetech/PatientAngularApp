a87eb0c4c3ba6fb60cb62ce0e570b18792fd684d
jobin <jobinjose.in@gmail.com>
2022-12-07 06:34:58 +0530

    service

--- src/app/view-patient/view-patient.component.ts
+++ src/app/view-patient/view-patient.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-view-patient',
@@ -7,8 +8,9 @@ import { Component } from '@angular/core';
 })
 export class ViewPatientComponent {
   constructor(private api:ApiService){
-      api.getPatients().subscribe(
+      api.viewPatients().subscribe(
         (response)=>{
+          console.log(response)
           this.patients = response;
         }
       )
